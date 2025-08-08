9006c6e27fdbef29734b37f1e0321f9621a6a15d
leiyuning <yuninglei.jonah@hotmail.com>
2025-08-08 15:50:01 +0800

    test

---
+++ Seed AI Studio 文档大纲.mm
@@ -0,0 +1,47 @@
+
+<map>
+  <node ID="root" TEXT="Seed AI Studio 文档大纲">
+    <node TEXT="Docs" ID="a7095f1cc39fafd6917075aff2f4b0b1" STYLE="bubble" POSITION="right">
+      <node TEXT="快速开始" ID="08338aa8f8f99da9027680a611e4cf76" STYLE="fork"/>
+      <node TEXT="模型列表" ID="bff43abd0539762706d0ceafb4e048b9" STYLE="fork"/>
+      <node TEXT="开发指南" ID="c224ce776a157b66173ddd7011a52fe4" STYLE="fork">
+        <node TEXT="深度思考" ID="b15565d40718a0ddb59d9dd6c47be9db" STYLE="fork"/>
+        <node TEXT="视觉理解" ID="033637570159db0eebf214d045511de2" STYLE="fork"/>
+        <node TEXT="图片生成" ID="ad2b9a36e83a219d294f7d5e61d0c7a4" STYLE="fork"/>
+        <node TEXT="视频生成" ID="af472a3ab66e94d04aa0fae864d3456b" STYLE="fork"/>
+        <node TEXT="文本生成" ID="3628245c65d2d880c7de11dfb998d2df" STYLE="fork"/>
+      </node>
+      <node TEXT="最佳实践" ID="0b875c910e1ed0f957d3b7d3ad07db39" STYLE="fork">
+        <node TEXT="生图提示词指南" ID="ed721ec19fe33bdb07570e476bf63bde" STYLE="fork"/>
+        <node TEXT="生视频提示词指南" ID="e9a007f48266f3f2bb5059e3d24fedcb" STYLE="fork"/>
+      </node>
+      <node TEXT="常见问题" ID="52af4d47ae017c08a23c2a0b04e81dbe" STYLE="fork"/>
+      <node TEXT="法务协议" ID="cb3c92fe06f9cbabcb5a5f6e5aeba0ec" STYLE="fork"/>
+    </node>
+    <node TEXT="API 文档" ID="148ea372a68ad8e826072d307048c00d" STYLE="bubble" POSITION="right">
+      <node TEXT="获取API Key" ID="4cc0b930526f6a022b63d6aad6eb1563" STYLE="fork"/>
+      <node TEXT="Base URL及鉴权" ID="1d01a96a3ae584d0f5fad18c323d5b02" STYLE="fork"/>
+      <node TEXT="错误码" ID="59a6e2dd7740f957cd369e356ed79571" STYLE="fork"/>
+      <node TEXT="限流" ID="f91654b496f8df1e62d29b53275e124b" STYLE="fork"/>
+      <node TEXT="Chat" ID="685ea4c26d3fdb1de9c8e43e1dcb0e5d" STYLE="fork"/>
+      <node TEXT="Responses" ID="c91b8fb19b6595f82720a45093c8eb92" STYLE="fork"/>
+      <node TEXT="Images Generation" ID="aada3ab3c0403c3dd8d451f1c8ba88c6" STYLE="fork">
+        <node TEXT="创建" ID="baf100554c2e1b7f7c164c786f10b69e" STYLE="fork"/>
+        <node TEXT="查询 by id" ID="b41af1d14ad6cab7a10972c6167636b4" STYLE="fork"/>
+        <node TEXT="查询 by filters" ID="52b6879ab0b315c669792c0f14521a28" STYLE="fork"/>
+        <node TEXT="删除 by id" ID="e6cfe349178f03e27a70bd527618c290" STYLE="fork"/>
+      </node>
+      <node TEXT="Contents Generations Task" ID="47628134dc4e06cad9c4662504f665fb" STYLE="fork"/>
+      <node TEXT="Bots" ID="90fff15fe1fff753444c1bde5be3c7ba" STYLE="fork">
+        <node TEXT="Bots Chat" ID="86f67dc5b4b257664e4049b046a3eb7c" STYLE="fork"/>
+        <node TEXT="插件" ID="af38375a915615d70e983b3c4c79a051" STYLE="fork">
+          <node TEXT="联网" ID="1a2a9af1bcfeb4fcc3599447ce9bcf14" STYLE="fork"/>
+          <node TEXT="文件解析？" ID="d41629ac663ee62669bdd685f815c1a8" STYLE="fork"/>
+        </node>
+      </node>
+    </node>
+    <node TEXT="ReleaseNotes" ID="6a23289851514337efbe74c2f0c38654" STYLE="bubble" POSITION="right">
+      <node TEXT="模型更新记录" ID="a5cf61faf4ef9c5d5dd5458ab3ddc778" STYLE="fork"/>
+    </node>
+  </node>
+</map>
